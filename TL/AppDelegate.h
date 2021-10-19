7cb05f937dadcbaad25045a8763594809eda4bb1
lichun03 <lichun03@kuaishou.com>
2021-10-19 20:13:30 +0800

    Initial Commit

---
+++ TL/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  TL
+//
+//  Created by lichun on 2021/10/19.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ TL/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  TL
+//
+//  Created by lichun on 2021/10/19.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
